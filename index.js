03c7633386f6155e4088772bfde769d00e98da22
Brad DeFauw <badefauw@gmail.com>
2023-03-02 11:55:27 -0600

    functional post route with bearer token

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const { PORT = 4000 } = process.env;
 // TODO - require express-openid-connect and destructure auth from it
 const { auth } = require('express-openid-connect');
+const jwt = require('jsonwebtoken');
 const { User, Cupcake } = require('./db');
 
 // middleware
@@ -22,6 +23,7 @@ app.use(express.urlencoded({extended:true}));
 // create a GET / route handler that sends back Logged in or Logged out
 
 const {
+  JWT_SECRET = 'neverTell',
   AUTH0_SECRET,
   AUTH0_AUDIENCE,
   AUTH0_CLIENT_ID,
@@ -29,7 +31,7 @@ const {
 } = process.env;
 
 const config = {
-  authRequired: true,
+  authRequired: false,
   auth0Logout: true,
   secret: AUTH0_SECRET,
   baseURL: AUTH0_AUDIENCE,
@@ -39,6 +41,66 @@ const config = {
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
+
+// createUser router
+app.use(async (req, res, next) => {
+  if (req.oidc.user) {
+    const [user] = await User.findOrCreate({
+      where: {
+        username: req.oidc.user.nickname,
+        name: req.oidc.user.name,
+        email: req.oidc.user.email,
+      }
+    });
+  }
+  next();
+});
+
+app.use(async (req, res, next) => {
+  try {
+    const auth = req.header('Authorization');
+    if (auth) {
+      const token = auth.split(' ')[1];
+      const decoded = jwt.verify(token, JWT_SECRET);
+      const user = await User.findByPk(decoded.id);
+      req.user = user;
+    }
+    next();
+  } catch (error) {
+    next(error);    
+  }
+});
+
+app.post('/cupcakes', async (req, res, next) => {
+  const { title, flavor, stars } = req.body;
+  if(req.user) {
+    const cupcake = await Cupcake.create({
+      title,
+      flavor,
+      stars,
+      userId: req.user.id,
+    });
+    res.json(cupcake);
+  } else {
+    res.status(401).send('You must be logged in to create a cupcake');
+  }
+});
+
+app.get('/me', async (req, res, next) => {
+  if(req.oidc.user) {
+    const user = await User.findOne({
+      where: {
+        username: req.oidc.user.nickname
+      },
+      raw: true
+    });
+    delete user.password;
+    const token = jwt.sign(user, JWT_SECRET, { expiresIn: '1w' });
+    res.json({user, token});
+  } else {
+    res.json({user: null, token: null});
+  }
+})
 
 app.get('/', (req, res) => {
   console.log(req.oidc.user);
